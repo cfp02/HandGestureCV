78ec64abf52c38de11dc8c45f2c52557b31e662e
Cole Parks <cparks@wpi.edu>
2023-09-27 01:10:21 -0400

    Working trained network, working on real-time classifier

---
+++ test0/collect_images.py
@@ -0,0 +1,63 @@
+import cv2
+import matplotlib.pyplot as plt
+import mediapipe as mp
+import os
+from datetime import datetime
+
+from header import DATA_DIR, number_of_classes, dataset_size, SPACEBAR, ESC
+
+
+cap : cv2.VideoCapture = None
+
+def check_directories():
+    if not os.path.exists(DATA_DIR):
+        os.makedirs(DATA_DIR)
+
+def record_photos():
+    # Record video to get images to train each class
+    cap = cv2.VideoCapture(0)
+    for j in range(number_of_classes):
+        if not os.path.exists(os.path.join(DATA_DIR, str(j))):
+            os.makedirs(os.path.join(DATA_DIR, str(j)))
+
+        print('Collecting data for class {}'.format(j))
+        done = False
+        while True:
+            ret, frame = cap.read()
+            cv2.putText(frame, 'Ready? Press "Space"', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
+                        cv2.LINE_AA)
+            cv2.imshow('frame', frame)
+            # Get key pressed
+            key_pressed = cv2.waitKey(1) & 0xFF
+            if key_pressed == SPACEBAR:
+                print("Starting")
+                break
+            # If esc pressed
+            elif key_pressed == ESC:
+                print("Quitting")
+                end_program()
+
+        counter = 0
+        while counter < dataset_size:
+            ret, frame = cap.read()
+            cv2.imshow('frame', frame)
+            k = cv2.waitKey(25)
+            if k == 27:
+                print("Quitting")
+                end_program()
+            cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
+            counter += 1
+
+def end_program():
+    cv2.destroyAllWindows()
+    exit()
+
+def setup():
+    check_directories()
+
+def main():
+    setup()
+    record_photos()
+
+if __name__ == '__main__':
+    main()
---
+++ test0/create_landmark_dataset.py
@@ -0,0 +1,64 @@
+import os
+
+import mediapipe as mp
+import cv2
+import matplotlib.pyplot as plt
+from mediapipe.python.solutions import hands as mp_hands, drawing_utils as mp_drawing, drawing_styles as mp_drawing_styles
+
+from header import DATA_DIR, WORKING_DIR, number_of_classes, dataset_size, SPACEBAR, ESC
+
+# mp.solutions imports other packages, so links to the hands class do not work with VS code
+# mp_hands = hands
+# mp_drawing = drawing_utils
+# mp_drawing_styles = drawing_styles
+
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+landmark_dataset = []
+landmark_labels = []
+
+# For each class directory
+for dir_ in os.listdir(DATA_DIR):
+    # For each image in the class folder
+    for img_path in os.listdir(os.path.join(DATA_DIR, dir_)):
+
+        data_aux = []
+        x_ = []
+        y_ = []
+
+        img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        from mediapipe.framework.formats import landmark_pb2
+
+        results= hands.process(img_rgb)
+        if results.multi_hand_landmarks:
+            if(len(results.multi_hand_landmarks) > 1):
+                print("More than one hand detected in " + img_path)
+                continue
+            for hand_landmarks in results.multi_hand_landmarks:
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+
+                    x_.append(x)
+                    y_.append(y)
+
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    data_aux.append(x - min(x_))
+                    data_aux.append(y - min(y_))
+
+            landmark_dataset.append(data_aux)
+            landmark_labels.append(dir_)
+
+print("Finished creating landmark dataset")
+
+# for i in range(len(landmark_dataset)):
+#     print(i, len(landmark_dataset[i]))
+
+import pickle
+with open(WORKING_DIR + '\\landmark_dataset.pickle', 'wb') as f:
+    pickle.dump({'data': landmark_dataset, 'labels': landmark_labels}, f)
+
+print("Wrote landmark dataset to " + WORKING_DIR + '\\landmark_dataset.pickle')
