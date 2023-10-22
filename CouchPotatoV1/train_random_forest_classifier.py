f5ebadcafc73db552feb54ba20bc9472cd959954
Cole Parks <cparks@wpi.edu>
2023-10-21 22:52:17 -0400

    Added micromlgen to random forest for exporting to C++

--- CouchPotatoV1/train_random_forest_classifier.py
+++ CouchPotatoV1/train_random_forest_classifier.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from micromlgen import port
 
 from header import DATA_DIR, WORKING_DIR, dataset_size, SPACEBAR, ESC
 
@@ -28,10 +29,17 @@ def test_classifier(model: RandomForestClassifier, x_test, y_test):
     score = accuracy_score(y_predict, y_test)
     print('{}% of samples were classified correctly !'.format(score * 100))
 
-def save_model(model, model_path = WORKING_DIR + '\\model_tflite.p'):
+def save_model(model, model_path = WORKING_DIR + '\\random_forest_model.p'):
     f = open(model_path, 'wb')
     pickle.dump({'model': model}, f)
     f.close()
+
+# Creates .h model for use on an Arduino
+def export_model_to_arduino(model, model_path = WORKING_DIR + '\\random_forest_model.h'):
+    random_forest_arduino_c = port(model)
+    with open(model_path, 'w') as f:
+        f.write(random_forest_arduino_c)
+        print("Model exported to " + model_path + " for use on an Arduino")
 
 def train_random_forest_classifier():
     landmark_data, landmark_labels = load_data()
@@ -39,6 +47,7 @@ def train_random_forest_classifier():
     model = train_classifier(x_train, y_train)
     test_classifier(model, x_test, y_test)
     save_model(model)
+    export_model_to_arduino(model)
 
 if __name__ == '__main__':
     train_random_forest_classifier()
